967526f51e08efec71a05e50c8430ebd371cbc32
arroyhondo <garysanchez012@gmail.com>
2021-10-23 13:09:44 -0400

    emitting and resizing .jpgs

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,35 @@ module.exports = {
     app: "./assets/js/script.js",
     events: "./assets/js/events.js",
     schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
+    tickets: "./assets/js/tickets.js",
   },
   output: {
-    path: path.join(__dirname + "/dist"),
     filename: "[name].bundle.js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name(file) {
+                return "[path][name].[ext]";
+              },
+              publicPath: function (url) {
+                return url.replace("../", "/assets/");
+              },
+            },
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ],
+      },
+    ],
   },
   plugins: [
     new webpack.ProvidePlugin({
